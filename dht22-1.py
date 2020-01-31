435fb81de4cd08d9ca11d0abc4f2d0cb9684cfa8
Elfatih Khalid <60464409+elfatihcrypto@users.noreply.github.com>
2020-01-31 12:20:15 +0400

    Create dht22-1.py

---
+++ dht22-1.py
@@ -0,0 +1,32 @@
+import time
+import datetime
+
+import Adafruit_DHT
+
+timestamp = datetime.datetime.now()
+
+current_date = timestamp.strftime("%d" "-" "%m" "-" "%Y")
+current_time = timestamp.strftime("%H" ":" "%M" ":" "%S")
+
+print (timestamp)
+print (current_date)
+print (current_time)
+
+
+sensor_name = Adafruit_DHT.DHT22
+sensor_pin = 23
+
+
+while 1:
+    timestamp = datetime.datetime.now()
+    current_time = timestamp.strftime("%H" ":" "%M" ":" "%S")
+ 
+    humidity, temperature = Adafruit_DHT.read_retry(sensor_name, sensor_pin)
+  	temp1 = '{0:0.2f}*C'.format(temperature, humidity)
+    humid1 = '{1:0.2f}%'.format(temperature, humidity)
+
+    payload = '{ "timestamp": "' + current_time + '","temperature": ' + str(temp1) + ',"humidity": '+ str(humid1) + ' }'
+
+    print payload
+    
+    time.sleep(2)
